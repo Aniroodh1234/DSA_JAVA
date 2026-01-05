3e81c03240c9d0ecf98fa3601d1c786e60404510
[firstname lastname] <[aniroodhroh2020@gmail.com]>
2026-01-06 00:03:43 +0530

    Datatypes in java

---
+++ java_basics/variables_&_datatypes/datatype.java
@@ -0,0 +1,24 @@
+// Java is a typed language: --//> means we have to specify the data type of a variable while declaring it.
+
+public class datatype{
+    public static void main(String[]args){
+        int a = 5;              //size:- 4 bytes
+        boolean out = true;     //size:- 1 bytes
+        char grade = 'A';       //size:- 2 bytes
+        float b = 5.67f;        //size:- 4 bytes
+        double c = 67.484;      //size:- 8 bytes
+        long d = 4265436;       //size:- 4 bytes
+        byte z = 22;            //size:- 1 bytes
+        String name = "Aniroodh";
+        System.out.println("Integer value: " + a);
+        System.out.println("Boolean value: " + out);
+        System.out.println("Character value: " + grade);
+        System.out.println("Float value: " + b);
+        System.out.println("Double value: " + c);
+        System.out.println("Long value: " + d);
+        System.out.println("String value: " + name);
+        System.out.println("Byte value: " + z);
+
+        
+    }
+}

e1c8232821001e7bf8564c2f8e8db4464013eb32
[firstname lastname] <[aniroodhroh2020@gmail.com]>
2026-01-06 23:46:06 +0530

    unary operator in java

---
+++ java_basics/operators/unary_operator.java
@@ -0,0 +1,28 @@
+// unary_operator --> The operator where we are having one operator and one operand
+public class unary_operator{
+    public static void main(String[]args){
+        //post increment
+        // int a = 10;
+        // int b = a++;
+        // System.out.println(a);
+        // System.out.println(b);
+
+        // pre increment
+        // int a = 10;
+        // int b = ++a;
+        // System.out.println(a);
+        // System.out.println(b);
+
+        //post decrement
+        // int a = 10;
+        // int b = a--;
+        // System.out.println(a);
+        // System.out.println(b);
+
+        //pre decrement
+        int a = 10;
+        int b = --a;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}

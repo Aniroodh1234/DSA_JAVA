e0596e7ed93f38280604111e01373206628906dc
[firstname lastname] <[aniroodhroh2020@gmail.com]>
2026-01-06 23:45:48 +0530

    relational operators

---
+++ java_basics/operators/relational_operator.java
@@ -0,0 +1,13 @@
+public class relational_operator{
+    public static void main(String[]args){
+        int a = 10;
+        int b = 20;
+        System.out.println(a > b);  // greater than
+        System.out.println(a < b);  // less than
+        System.out.println(a >= b); // greater than or equal to
+        System.out.println(a <= b); // less than or equal to
+        System.out.println(a == b); // equal to
+        System.out.println(a != b); // not equal to
+        
+    }
+}

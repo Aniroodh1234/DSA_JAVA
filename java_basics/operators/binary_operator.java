4434073a8a42bf100e3b85615a9b5fdb916773a1
[firstname lastname] <[aniroodhroh2020@gmail.com]>
2026-01-06 23:44:52 +0530

    binary operators

---
+++ java_basics/operators/binary_operator.java
@@ -0,0 +1,9 @@
+//  binary_operator --> The operator where we are having one operator and two operands
+
+public class binary_operator{
+    public static void main(String[]args){
+        int a = 10;
+        int b= 20;
+        System.out.println("The sum is: "+ (a+b));
+    }
+}

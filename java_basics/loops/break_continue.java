cab1a5763fd18a05a02d1fe0bce808ba891c16db
[firstname lastname] <[aniroodhroh2020@gmail.com]>
2026-01-07 23:51:41 +0530

    break_conntinue statements

---
+++ java_basics/loops/break_continue.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class break_continue {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+
+        do{
+            int n = sc.nextInt();
+            if(n % 10 ==0){
+                break;
+            }
+        }while(true); 
+            }
+        }

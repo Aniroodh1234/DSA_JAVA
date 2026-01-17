79fd9382facc226c710b246524f5f4df562fd849
[firstname lastname] <[aniroodhroh2020@gmail.com]>
2026-01-17 23:54:04 +0530

    prime number code

--- java_basics/loops/prime_no.java
+++ java_basics/loops/prime_no.java
@@ -1,23 +1,23 @@
-import java.util.*;
+import java.util.Scanner;
 public class prime_no{
-    public static void main(String[] args) {
+    public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number: ");
+        System.out.print("Enter the number to check for the prime number: ");
         int n = sc.nextInt();
-        if(n==1){
-            System.out.println("1 is neither prime nor composite number.");
-            return;
+        if(n == 1){
+            System.out.print("1 is neither prime nor composite number.");
         }
-        boolean isPrime = true;
-        for(int i = 2; i <= Math.sqrt(n); i++){
-            if(n % i ==0){
-                isPrime = false;
+        boolean is_prime = true;
+        for(int i =2; i<= Math.sqrt(n);i++){
+            if(n%i==0){
+                is_prime = false;
             }
         }
-        if(isPrime==true){
-            System.out.println(n + " is a prime number.");
-        } else {
-            System.out.println(n + " is not a prime number.");
+        if(is_prime == true){
+            System.out.print("" + n + " is a prime number.");
+        }
+        else{
+            System.out.print("" + n + " is not a prime number.");
         }
     }
 }

490eeffba06537778052422a3885403598c5d55d
Aniroodh1234 <aniroodhroh2020@gmail.com>
2026-02-23 23:23:22 +0530

    Operators in java

---
+++ java_basics/2_operators/assignment_operator.java
@@ -0,0 +1,12 @@
+public class assignment_operator{
+    public static void main(String[]args){
+        int a = 10;
+        a += 5; // a = a + 5
+        a -= 3; // a = a - 3
+        a *= 2; // a = a * 2
+        a /= 4; // a = a / 4
+        a %= 3; // a = a % 3
+        System.out.println("The final value of a is: " + a);
+        
+    }
+}

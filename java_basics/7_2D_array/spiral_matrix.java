fed10db1be22e5323a9fac0169e91c9a798213de
Aniroodh1234 <aniroodhroh2020@gmail.com>
2026-03-04 16:06:56 +0530

    spiral matrix

---
+++ java_basics/7_2D_array/spiral_matrix.java
@@ -0,0 +1,43 @@
+import java.util.*;
+public class spiral_matrix{
+    public static void spiralmatrix(int arr[][]){
+        int startrow = 0;
+        int endrow = arr.length-1;
+        int startcol = 0;
+        int endcol = arr[0].length-1;
+
+        while(startrow<=endrow && startcol<=endcol){
+            for(int j = startcol; j<=endcol;j++){
+                System.out.print(arr[startrow][j]+" ");
+            }
+
+            for(int i=startrow+1;i<=endrow;i++){
+                System.out.print(arr[i][endcol]+" ");
+            }
+
+            for(int j = endcol-1;j>=startrow;j--){
+                System.out.print(arr[endrow][j]+" ");
+            }
+
+            for(int i=endrow-1;i>=startrow+1;i--){
+                System.out.print(arr[i][startcol]+" ");
+            }
+            startrow++;
+            endcol--;
+            endrow--;
+            startcol++;
+        }
+    }
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int matrix[][] = {
+            {1,2,3,4},
+            {5,6,7,8},
+            {9,10,11,12},
+            {13,14,15,16}
+        };
+        spiralmatrix(matrix);
+
+
+    }
+}

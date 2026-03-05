0766cbe0de831a4c13c4cf7f026e263ea386311b
Aniroodh1234 <aniroodhroh2020@gmail.com>
2026-03-05 13:13:43 +0530

    insertion sort

---
+++ java_basics/8_sorting/insertionsort.java
@@ -0,0 +1,26 @@
+// its a inplace sorting algorithm
+
+public class insertionsort{
+    public static void sort(int arr[]){
+        for(int i = 1; i<arr.length;i++){
+            int curr = arr[i];
+            int prev = i-1;
+            while(prev>=0 && curr<arr[prev]){
+                arr[prev+1] = arr[prev];
+                prev--;
+            }
+            arr[prev+1] = curr;
+        }
+    }
+    public static void print(int arr[]){
+        for(int i = 0; i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void main(String[]args){
+        int arr[] = {5,4,1,3,2};
+        // print(arr);
+        sort(arr);
+        print(arr);
+    }
+}

89ae7381b6b0fc5a557622af0e6e342c02f46c74
Aniroodh1234 <aniroodhroh2020@gmail.com>
2026-03-14 09:57:14 +0530

    string concatination

---
+++ java_basics/9_String/stringconcat.java
@@ -0,0 +1,7 @@
+public class stringconcat{
+    public static void main(String[] args) {
+        String str1 = "Aniroodh";
+        String str2 = "Padhee";
+        System.out.print(str1+" "+str2);
+    }
+}

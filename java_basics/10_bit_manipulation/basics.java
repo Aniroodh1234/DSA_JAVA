b727b6de5eec05063ce80ef31251669adc4cd97f
Aniroodh1234 <aniroodhroh2020@gmail.com>
2026-03-16 10:22:27 +0530

    basics of bit manipulation

---
+++ java_basics/10_bit_manipulation/basics.java
@@ -0,0 +1,14 @@
+public class basics{
+    public static void main(String[]args){
+        // operators in bit manipulation
+        // &, |, >>, <<, 1~s complement
+
+        System.out.println(4&5);
+        System.out.println(4|5);
+        System.out.println(10<<2);
+        System.out.println(10>>2);
+        System.out.println(~5);
+
+
+    }
+}
